51460bb5d24add34fd2b2a6e58b377ba6a81c2fe
hallma886 <hallma886@northwested.org>
2025-02-24 13:40:54 -0500

    basic_shapes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Draw Lines in Pygame / No Functions
+# Draw 
 
 # Pygame game template
 
@@ -9,7 +9,7 @@ import config  # Import the config module
 def init_game():
     pygame.init()
     screen = pygame.display.set_mode((config.WINDOW_WIDTH, config.WINDOW_HEIGHT))  # Use constants from config
-    pygame.display.set_caption(TITLE)
+    pygame.display.set_caption(config.TITLE)
     return screen
 
 def handle_events():
@@ -20,6 +20,15 @@ def handle_events():
             if event.key == pygame.K_ESCAPE:
                 return False  # Return False to indicate quitting
     return True  # Continue running if no quit event
+
+def draw_rectangle(screen, rect, color, thickness):
+    pygame.draw.rect(screen, color, rect, thickness)
+
+def draw_circle(screen, center, radius, color, thickness):
+    pygame.draw.circle(screen, color, center, radius, thickness)
+
+def draw_line(screen, color, start_pos, end_pos, thickness):
+    pygame.draw.line(screen, color, start_pos, end_pos, thickness)
 
 def main():
 
@@ -35,6 +44,24 @@ def main():
 
         # Fill the screen with a background color 
         screen.fill(config.WHITE) 
+
+        draw_line(screen, config.BLACK, [100, 300], [400, 400], 5)
+
+        draw_line(screen, config.GREEN, [350, 150], [300, 500], 8)
+
+        draw_rectangle(screen, [100, 100, 200, 150], config.BLUE, 0)
+
+        draw_rectangle(screen, [200, 200, 200, 150], config.ORANGE, 0)
+
+        draw_rectangle(screen, [300, 300, 200, 150], config.PURPLE, 0)
+
+        draw_rectangle(screen, [400, 400, 200, 150], config.RED, 0)
+
+        draw_circle(screen, [537, 200], 75, config.FOREST_GREEN, 0)
+
+        draw_circle(screen, [394, 200], 75, config.BROWN, 0)
+
+        draw_circle(screen, [455, 200], 75, config.YELLOW, 0)
 
         pygame.display.flip()  # Update the display
 
